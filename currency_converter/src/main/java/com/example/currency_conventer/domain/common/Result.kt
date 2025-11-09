0ce3bfb7cbc43c6970adf6aea75cfda1666c0aae
PawelZielinski <pawel.zielinski@bitapps.fi>
2025-11-09 13:43:18 +0100

    ADD: Result wrapper

---
+++ currency_converter/src/main/java/com/example/currency_conventer/domain/common/Result.kt
@@ -0,0 +1,17 @@
+package com.example.currency_conventer.domain.common
+
+sealed class Result<out T> {
+    data class Success<T>(val data: T) : Result<T>()
+    data class Error(val exception: Exception, val message: String? = null) : Result<Nothing>()
+    data object Loading : Result<Nothing>()
+}
+
+inline fun <T> Result<T>.onSuccess(action: (T) -> Unit): Result<T> {
+    if (this is Result.Success) action(data)
+    return this
+}
+
+inline fun <T> Result<T>.onError(action: (Exception, String?) -> Unit): Result<T> {
+    if (this is Result.Error) action(exception, message)
+    return this
+}
