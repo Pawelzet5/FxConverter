d5d02143d667bdcd5e7194c99a6578e483ad4b3d
PawelZielinski <pawel.zielinski@bitapps.fi>
2025-11-13 14:40:02 +0100

    build: Setting up hilt in app module

--- app/src/main/java/com/example/fxconverter/MainActivity.kt
+++ app/src/main/java/com/example/fxconverter/MainActivity.kt
@@ -4,16 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.currency_conventer.presentation.ui.ConversionScreenRoot
 import com.example.fxconverter.ui.theme.FxConverterTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            FxConverterTheme {
-
-            }
+            ConversionScreenRoot()
         }
     }
 }
