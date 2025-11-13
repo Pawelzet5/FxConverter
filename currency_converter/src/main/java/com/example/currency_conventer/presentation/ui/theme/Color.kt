35d89cc0009b5d190f5eef033731d474487c099c
PawelZielinski <pawel.zielinski@bitapps.fi>
2025-11-13 14:46:41 +0100

    ADD: color values

---
+++ currency_converter/src/main/java/com/example/currency_conventer/presentation/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.example.currency_conventer.presentation.ui.theme
+import androidx.compose.ui.graphics.Color
+
+
+val primaryColor = Color(0xFF317FF5)
+val disabledColor = Color(0xFFEDF0F4)
+
+val secondaryTextColor = Color(0xFF6C727A)
+
+val errorColor = Color(0xFFF8326A)
+val warningSurfaceColor = Color(0x1AE5476D)
+val warningTextColor = Color(0xFFE5476D)
+
+val dialogHandleColor = Color(0xFFE6E1E6)
